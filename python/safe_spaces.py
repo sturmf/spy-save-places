7f2f5d977924b5084f8e0f91c56017c5b58ae2f8
Christian Gesell <christian.gesell@t-online.de>
2019-02-01 11:13:24 +0100

    Refactoring

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -4,14 +4,31 @@ from itertools import repeat, product
 # Questions
 # IODA: When to make a step function private? What about the tests at that point?
 # IODA: Should it not be allowed to extract functions from pure functions?
+# IODA: When to introduce classes?
+# IODA: When to crefactory primitive types to explicit data types?
+# IODA: Thoughts on early returns? Usually an if statement and a return statement - IODA violation?
 
 # Refactorings
+# Introduce space data type with coordinates and min distance
 # Don't use -1 as Marker, MAX_DISTANCE, or NO_AGENT_IN_SIGHT would be nicer
 # Pull all occurences of map size information out of methods to a single place
 # Add test for spies on the map edge to catch +-1 errors
 # Correct expected actual ordering in assert calls
+# Switch to iterator objects
 
-
+# find save spaces
+#
+# Second Idea
+# generate initial locations
+# apply each agent to all locations
+# reduce to safe spaces
+# convert to response model
+#
+# First Idea
+# convert_agents_to_distance_maps
+# collate_map_fields_to_minima
+# find_safest_distance
+# convert_safest_fields_to_coordinates
 
 class SafetyFinder:
     """A class that contains everything we need to find the
@@ -33,8 +50,7 @@ class SafetyFinder:
             'K': 10,
         }
 
-        self.int_to_char = inv_map = {v: k for k, v in self.char_to_int.items()}
-
+        self.int_to_char = {v: k for k, v in self.char_to_int.items()}
 
     def convert_coordinates(self, agents):
         """This method should take a list of alphanumeric coordinates (e.g. 'A6')
@@ -47,6 +63,7 @@ class SafetyFinder:
         Returns a list of coordinates in zero-indexed vector form.
         """
         return [self._agent_to_coordinates(agent) for agent in agents]
+        return list(map(self._agent_to_coordinates, agents)) # TODO
 
     def _agent_to_coordinates(self, agent):
         return [self.char_to_int[agent[0]], int(agent[1:]) - 1]
@@ -62,23 +79,16 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
+        initial_spaces = self._generate_initial_spaces(10, 10)
+        spaces_with_agents = self._place_agents(agents, initial_spaces)
+        safe_spaces = self._filter_to_safe_spaces(spaces_with_agents)
 
-        # generate initial locations
-        spaces = self._generate_initial_spaces(10, 10)
-        # apply each agent to all locations
-        for agent in agents:
-            # place agent
-            spaces = self._place_agent(spaces, agent)
-        # reduce to safe spaces
-        safe_spaces = self._filter_to_safe_spaces(spaces)
-        # convert
         return self._convert_to_list_of_spaces(safe_spaces)
 
-        # Other Idea
-        # convert_agents_to_distance_maps
-        # collate_map_fields_to_minima
-        # find_safest_distance
-        # convert_safest_fields_to_coordinates
+    def _place_agents(self, agents, spaces):
+        for agent in agents:
+            spaces = self._place_agent(spaces, agent)
+        return spaces
 
     def _generate_initial_spaces(self, x_length, y_length):
         coordinates = set(product(range(x_length), range(y_length)))
@@ -88,25 +98,27 @@ class SafetyFinder:
         updated_spaces = set()
         (x_agent, y_agent) = agent
 
-        # for each space
         for space in spaces:
             (x_space, y_space, current_distance) = space
-            # calc distance to agent
             distance_to_agent = abs(x_agent - x_space) + abs(y_agent - y_space)
-            # update space with highest threat = lowest number
             updated_distance = distance_to_agent if current_distance == -1 else min(current_distance, distance_to_agent)
             updated_spaces.add((x_space, y_space, updated_distance))
 
         return updated_spaces
 
     def _filter_to_safe_spaces(self, spaces):
-        (x, y, max_distance) = max(spaces, key=lambda x: x[2])
+        (x, y, max_distance) = max(spaces, key=lambda s: s[2])
+
+        if max_distance == 0:
+            return []
+
         safe_spaces = {space for space in spaces
                        if
-                       space[2] == max_distance and
-                       space[2] != 0
+                       space[2] == max_distance
                        }
         return safe_spaces
+
+        return set(filter(lambda s: s[2] == max_distance, spaces)) # TODO
 
     def _convert_to_list_of_spaces(self, spaces):
         return [[x, y] for (x , y, d) in spaces]
@@ -123,8 +135,6 @@ class SafetyFinder:
         Returns either a list of alphanumeric map coordinates for Alex to hide in,
         or a specialized message informing her of edge cases
         """
-
-
         agents_coordinates = self.convert_coordinates(agents)
         agents_coordinates = self._remove_agents_outside_map(agents_coordinates)
 
